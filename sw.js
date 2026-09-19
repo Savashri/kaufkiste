3b20fa25b84c97cb02f14404ca6085d932a5cd5e
Savashri <d.reintges@googlemail.com>
2026-09-19 23:20:07 +0200

    Bump PWA cache for token settings

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "kaufkiste-pwa-v3";
+const CACHE_NAME = "kaufkiste-pwa-v4";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=3",
-  "./app.js?v=3",
-  "./manifest.webmanifest?v=3",
+  "./styles.css?v=4",
+  "./app.js?v=4",
+  "./manifest.webmanifest?v=4",
   "./icons/icon.svg",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
