3ebcc930d0ee466f67b75a1034101ce83fb27075
Savashri <d.reintges@googlemail.com>
2026-09-19 22:44:43 +0200

    Bust PWA cache for remote storage release

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "kaufkiste-pwa-v2";
+const CACHE_NAME = "kaufkiste-pwa-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
+  "./styles.css?v=3",
+  "./app.js?v=3",
+  "./manifest.webmanifest?v=3",
   "./icons/icon.svg",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
